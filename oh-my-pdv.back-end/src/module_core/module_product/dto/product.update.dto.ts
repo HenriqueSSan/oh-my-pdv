2cd808a3e69ce4073e83dea0dc534267da508228
HenriqueSSan <hsstudentwork@gmail.com>
2026-03-19 17:02:03 -0300

    feat: added product update dto

---
+++ oh-my-pdv.back-end/src/module_core/module_product/dto/product.update.dto.ts
@@ -0,0 +1,13 @@
+import { Decimal } from '@prisma/client/runtime/library';
+
+export class ProductUpdateDto {
+  constructor(
+    public name: string,
+    public description: string,
+    public base_price: Decimal,
+  ) {}
+
+  valueOf() {
+    return this;
+  }
+}
